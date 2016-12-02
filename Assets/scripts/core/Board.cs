4b7b8955588acea1d23ddf1863a3db19bdd76b95
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-02 23:20:41 +0000

    Hard code camera position and add header to grid

--- Assets/scripts/core/Board.cs
+++ Assets/scripts/core/Board.cs
@@ -6,23 +6,18 @@ public class Board : MonoBehaviour {
 	[SerializeField]
 	private Transform emptyCellPrefab;
 	[SerializeField]
-	private int boardWidth, boardHeight;
+	private int boardWidth, boardHeight, header;
 
 	private void Awake () {
 		Assert.IsNotNull(emptyCellPrefab);
 	}
 
 	private void Start () {
-		SetCameraPosition();
 		DrawEmptyCells();
 	}
 
-	private void SetCameraPosition () {
-		Camera.main.transform.position = new Vector3((float)(boardWidth - 1) / 2, (float)(boardHeight - 1) / 2, -10);
-	}
-
 	private void DrawEmptyCells () {
-		for (int y = 0; y < boardHeight; y++) {
+		for (int y = 0; y < boardHeight - header; y++) {
 			for (int x = 0; x < boardWidth; x++) {
 				Transform clone;
 				clone = Instantiate (emptyCellPrefab, new Vector3(x, y, 0), Quaternion.identity) as Transform;

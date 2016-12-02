69ef883c68d8ed7c86acb386de5588bc5ba66a77
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-02 23:09:34 +0000

    Create empty square prefab and create method to create an empty board on Start

--- Assets/scripts/core/Board.cs
+++ Assets/scripts/core/Board.cs
@@ -2,6 +2,24 @@
 
 public class Board : MonoBehaviour {
 
-	
+	[SerializeField]
+	private Transform emptyCellPrefab;
+	[SerializeField]
+	private int boardWidth, boardHeight;
+
+	private void Start () {
+		DrawEmptyCells();
+	}
+
+	private void DrawEmptyCells () {
+		for (int y = 0; y < boardHeight; y++) {
+			for (int x = 0; x < boardWidth; x++) {
+				Transform clone;
+				clone = Instantiate (emptyCellPrefab, new Vector3(x, y, 0), Quaternion.identity) as Transform;
+				clone.name = "Board space (x = " + x.ToString() + ", y = " + y.ToString() + ")";
+				clone.transform.parent = transform;
+			}
+		}
+	}
 
 }

5bab565d339b7c1e39a2858d55cfe38c7e3d7b45
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-03 20:07:29 +0000

    Add movement methods to Shape class (and fix pivot point of S tetromino

--- Assets/scripts/core/Shape.cs
+++ Assets/scripts/core/Shape.cs
@@ -1,16 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Shape : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	[SerializeField]
+	private bool isRotatable;
+
+	private void Start () {
+
+	}
+
+	private void Move (Vector3 moveDirection) {
+		transform.position += moveDirection;
+	}
+
+	private void MoveLeft () {
+		Move(new Vector3(-1, 0, 0));
+	}
+
+	private void MoveRight () {
+		Move(new Vector3(1, 0, 0));
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	private void MoveUp () {
+		Move(new Vector3(0, 1, 0));
+	}
+	
+	private void MoveDown () {
+		Move(new Vector3(0, -1, 0));
+	}
+
+	private void RotateLeft () {
+		if (isRotatable) {
+			transform.Rotate(0, 0, 90);
+		}
+	}
+
+	private void RotateRight () {
+		if (isRotatable) {
+			transform.Rotate(0, 0, -90);
+		}
 	}
 }

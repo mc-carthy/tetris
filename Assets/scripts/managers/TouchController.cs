14e019af33424815d9e84a21b6a470c350f7386b
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-07 12:39:45 +0000

    Create TouchController

---
+++ Assets/scripts/managers/TouchController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class TouchController : MonoBehaviour {
+
+	public delegate void TouchScreenEventHandler (Vector2 swipe);
+	
+	public static event TouchScreenEventHandler SwipeEvent;
+
+	private Vector2 touchMovement;
+	private int minSwipeDistance = 20;
+
+	private void OnSwipe () {
+		if (SwipeEvent != null) {
+			SwipeEvent(touchMovement);
+		}
+	}
+
+	private void Update () {
+		if (Input.touchCount > 0) {
+			Touch touch = Input.touches[0];
+
+			if (touch.phase == TouchPhase.Began) {
+				touchMovement = Vector2.zero;
+			} else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary) {
+				touchMovement += touch.deltaPosition;
+
+				if (touchMovement.magnitude > minSwipeDistance) {
+					OnSwipe();
+				}
+			}
+		}
+	}
+
+}
